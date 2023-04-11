a8fcaae714291cfa1ec1bb5d9db0583da6da9450
Dhananjay Singh <42992869+dhananjaysr26@users.noreply.github.com>
2023-04-11 21:03:33 +0530

    Added: Learn Routes

---
+++ src/pages/learn/index.tsx
@@ -0,0 +1,12 @@
+import ProcessFlow from "@/components/ProcessFlow/ProcessFlow";
+import { NextPage } from "next";
+
+const LearnHomePage:NextPage=()=>{
+    return(
+        <>
+        <ProcessFlow />
+        <ProcessFlow />
+      </>
+    )
+}
+export default LearnHomePage
