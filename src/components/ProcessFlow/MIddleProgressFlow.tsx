13eb2e8189c1598fefa980495ab2f9b7354fe1dd
Dhananjay Singh <42992869+dhananjaysr26@users.noreply.github.com>
2023-04-20 22:32:34 +0530

    code Refactored

--- src/components/ProcessFlow/MIddleProgressFlow.tsx
+++ src/components/ProcessFlow/MIddleProgressFlow.tsx
@@ -1,16 +1,21 @@
 import { useEffect, useRef, useState } from "react";
 
 const MiddleProcessFlow: React.FC = () => {
-  const [scrollPercentage, setScrollPercentage] = useState(40);
+  const [scrollPercentage, setScrollPercentage] = useState(0);
 
   useEffect(() => {
     const handleScroll = () => {
-      const { scrollTop, scrollHeight, clientHeight } =
+      const { scrollTop, scrollHeight, clientHeight, clientWidth } =
         document.documentElement;
       const percent = (scrollTop / (scrollHeight - clientHeight)) * 100;
       let per = parseInt(percent.toFixed(0));
-      const height = per > 50 && per < 60 ? per-25 :per-25;
-      setScrollPercentage(height);
+      let h: any = null;
+      if (clientHeight > 400) {
+        h = per > 50 && per < 60 ? per - 25 : per - 25;
+      } else {
+        h = per > 50 && per < 60 ? per - 45 : per - 45;
+      }
+      setScrollPercentage(h);
     };
 
     window.addEventListener("scroll", handleScroll);
@@ -20,13 +25,16 @@ const MiddleProcessFlow: React.FC = () => {
 
   console.log(scrollPercentage);
   return (
-    <div className="w-screen h-screen px-24 bg-slate-100 border-y-2 flex justify-center items-center flex-col">
+    <div className="w-screen h-screen px-4 md:px-24 bg-slate-100 border-y-2 flex justify-center items-center flex-col">
       <h1 className="text-black text-3xl text-center">Middle Progress</h1>
-      <div className="flex justify-center items-center">
+      <div className="flex justify-center items-center max-w-[100vw]">
         <img
           src="https://cdn.pixabay.com/animation/2022/08/22/06/27/06-27-28-539_512.gif"
-          className=" max-w-none max-h-none"
-          style={{ height: `${scrollPercentage}rem`, maxHeight: "100vh" }}
+          style={{
+            height: `${scrollPercentage}rem`,
+            width: `${scrollPercentage}rem`,
+            maxHeight: "100vh",
+          }}
         />
       </div>
     </div>
