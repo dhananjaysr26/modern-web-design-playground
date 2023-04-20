69689820de20b6ea50b5a26e610cce5e6cf4089d
Dhananjay Singh <42992869+dhananjaysr26@users.noreply.github.com>
2023-04-20 21:41:39 +0530

    code Refactore

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
 import { Inter } from "next/font/google";
 import ProcessFlow from "@/components/ProcessFlow/ProcessFlow";
+import MiddleProcessFlow from "@/components/ProcessFlow/MIddleProgressFlow";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
+      <ProcessFlow />
+      <ProcessFlow />
+      <ProcessFlow />
+      <MiddleProcessFlow/>
       <ProcessFlow />
       <ProcessFlow />
     </>
