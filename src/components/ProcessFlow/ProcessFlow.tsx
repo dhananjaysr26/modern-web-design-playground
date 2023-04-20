1d3a51f40476181ef7ade833faa59f30a67e8211
Dhananjay Singh <42992869+dhananjaysr26@users.noreply.github.com>
2023-04-20 23:03:46 +0530

    Added: mobile view

--- src/components/ProcessFlow/ProcessFlow.tsx
+++ src/components/ProcessFlow/ProcessFlow.tsx
@@ -23,7 +23,7 @@ const ProcessFlow: React.FC = () => {
     return () => window.removeEventListener("scroll", handleScroll);
   }, [prevScrollPos]);
   return (
-    <div className=" w-screen h-screen px-4 md:px-24 bg-slate-100 ">
+    <div className=" w-screen h-screen px-4 md:px-24 bg-slate-100 max-w-[1200px] mx-auto">
       <h1 className=" text-black text-3xl">Hello</h1>
       <img
         src="/gif/airplane.gif"
@@ -31,8 +31,8 @@ const ProcessFlow: React.FC = () => {
           isScrollingDown ? "rotate-180" : ""
         } sticky top-20 -translate-x-6 z-40 transition-all duration-[2000ms] ease-out`}
       />
-      <div className="flex h-full items-center justify-center ">
-        <div className=" w-[30%] flex flex-col border-black/40 border-l-2 h-[80%] justify-center pl-12 ">
+      <div className="flex md:h-full items-center flex-wrap justify-center ">
+        <div className=" md:w-[30%] flex flex-col border-black/40 border-l-2 h-[80%] justify-center pl-12 ">
           <h1 className=" text-black">Info</h1>
           <p className="text-black">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid
@@ -41,7 +41,7 @@ const ProcessFlow: React.FC = () => {
             repellendus voluptatum ipsam?
           </p>
         </div>
-        <div className=" w-[70%] flex  justify-center ">
+        <div className=" w-[70%] flex  justify-center h-full md:h-auto bg-slate-600 md:bg-transparent">
           <img
             src="https://i.pinimg.com/originals/a1/79/b5/a179b58b285b1f8baed84e9a10daea7e.gif"
             className={`${
